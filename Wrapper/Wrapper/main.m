8bec2fec05156026da9356915356e07949348d36
collard <collard@98277486-ac76-4d9b-89a9-98605d28aefd>
2013-09-18 19:00:44 +0000

    git-svn-id: https://dev.cs.uakron.edu/svn/iOS/001/students/emp36/Projects/Project1@50 98277486-ac76-4d9b-89a9-98605d28aefd

---
+++ Wrapper/Wrapper/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Wrapper
+//
+//  Created by Michael Collard on 9/16/13.
+//  Copyright (c) 2013 student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Wrapper.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
