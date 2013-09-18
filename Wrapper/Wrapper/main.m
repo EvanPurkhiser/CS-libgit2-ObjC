218c558a6069866b2c47a13c9df86459924e6ad7
emp36 <emp36@98277486-ac76-4d9b-89a9-98605d28aefd>
2013-09-18 20:46:49 +0000

    Testing committing with git to SVN
    git-svn-id: https://dev.cs.uakron.edu/svn/iOS/001/students/emp36/Projects/Project1@56 98277486-ac76-4d9b-89a9-98605d28aefd

--- Wrapper/Wrapper/main.m
+++ Wrapper/Wrapper/main.m
@@ -15,7 +15,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! -- Evan");
         
     }
     return 0;
