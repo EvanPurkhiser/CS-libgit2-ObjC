37da9b6f8e50e664b06a846ffba58a2edb884510
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-01 04:07:01 -0400

    Add properites of a commit

--- Wrapper/Wrapper/Commit.h
+++ Wrapper/Wrapper/Commit.h
@@ -2,4 +2,13 @@
 
 @interface Commit : NSObject
 
+@property NSString *message;
+@property NSString *author;
+@property NSString *commiter;
+@property NSDate   *commitDate;
+@property NSArray  *parents;
+
+// We would probably also want something here that represents
+// The tree, but I don't think we're going to get that advanched
+
 @end
