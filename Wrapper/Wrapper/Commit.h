b7bd6269d7e9ddf05f25b79f3a988e0670012668
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-10-01 04:58:42 -0400

    A little explanation about why Commit is a object

--- Wrapper/Wrapper/Commit.h
+++ Wrapper/Wrapper/Commit.h
@@ -11,4 +11,8 @@
 // We would probably also want something here that represents
 // The tree, but I don't think we're going to get that advanched
 
+// We would probably want to be able to construct commits here as well
+// Right now this would probably make more sense as a struct. But in a
+// real implimentation it certianlly could have some instance methods
+
 @end
